852d694adf45ee9eb3183b34aa49c1396bc79dbf
Candido Sales Gomes <candidosg@gmail.com>
2022-10-10 18:23:30 -0400

    added medium icon and auryo picture

--- src/components/Interviews.svelte
+++ src/components/Interviews.svelte
@@ -1,21 +1,39 @@
 <script lang="ts">
   import Github from "./icons/Github.svelte";
   import Linkedin from "./icons/Linkedin.svelte";
+  import Medium from "./icons/Medium.svelte";
   import Twitter from "./icons/Twitter.svelte";
 
-  const interviews = [
+  interface InterviewCompany {
+    name: string;
+    url: string;
+  }
+
+  interface Interviews {
+    imageUrl: string;
+    name: string;
+    company: InterviewCompany;
+    summary: string;
+    location: string;
+    twitterUrl?: string;
+    mediumUrl?: string;
+    githubUrl?: string;
+    linkedinUrl?: string;
+  }
+
+  const interviews: Array<Interviews> = [
     {
-      imageUrl: "/avatar-addy.jpeg",
+      imageUrl: "/auryo.png",
       name: "Auryo Jota",
       company: {
         name: "Escritor e Redator",
+        url: "",
       },
       summary:
         "Desde o título, a obra é carregada de referências. A região de Teresina, a capital do Piauí, também é conhecida como Chapada do Corisco devido às fortes trovoadas e constantes raios que caem durante as tempestades... A preocupação com questões sociais é um ponto central na obra. Desigualdade, preconceito, pessoas invisibilizadas (se ninguém vê, elas não existem, não incomodam), a normalização da violência sofrida por determinados corpos.",
       location: "Teresina, Piauí",
-      githubUrl: "https://github.com/addyosmani",
-      twitterUrl: "https://twitter.com/addyosmani",
-      linkedinUrl: "https://linkedin.com/in/osmani",
+      twitterUrl: "https://twitter.com/auryo_j",
+      mediumUrl: "https://medium.com/@auryoj",
     },
   ];
 </script>
@@ -26,7 +44,7 @@
       Incluindo Entrevistas com...
     </h2>
     <div class="mt-6 pt-10">
-      {#each interviews as { name, imageUrl, company, summary, location, githubUrl, twitterUrl, linkedinUrl }}
+      {#each interviews as { name, imageUrl, company, summary, location, twitterUrl, githubUrl, linkedinUrl, mediumUrl }}
         <div class="flex-none sm:flex mb-12">
           <img class="w-40 h-40 rounded-full mr-8" src={imageUrl} alt={name} />
           <div class="text-sm">
@@ -43,9 +61,26 @@
             </p>
             <p class="text-sm italic text-gray-500  font-body">{location}</p>
             <div class="flex flex-row my-2">
-              <a href={githubUrl} target="_blank"><Github /> </a>
-              <a href={twitterUrl} target="_blank"><Twitter /></a>
-              <a href={linkedinUrl} target="_blank"><Linkedin /></a>
+              {#if githubUrl}
+                <a title="Github link" href={githubUrl} target="_blank"
+                  ><Github />
+                </a>
+              {/if}
+              {#if twitterUrl}
+                <a title="Twitter link" href={twitterUrl} target="_blank"
+                  ><Twitter />
+                </a>
+              {/if}
+              {#if linkedinUrl}
+                <a title="Linkedin link" href={linkedinUrl} target="_blank"
+                  ><Linkedin />
+                </a>
+              {/if}
+              {#if mediumUrl}
+                <a title="Medium link" href={mediumUrl} target="_blank"
+                  ><Medium />
+                </a>
+              {/if}
             </div>
           </div>
         </div>
