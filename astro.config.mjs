7165dded195e12b3db885d0eaf2c8a32e3d1884c
Candido Sales Gomes <candidosg@gmail.com>
2022-10-12 00:54:48 -0400

    test edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 import svelte from "@astrojs/svelte";
 
