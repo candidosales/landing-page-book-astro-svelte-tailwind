848707a64db8b38f1176c7dff57619c727dc31a7
Candido Sales Gomes <candidosg@gmail.com>
2022-10-12 17:08:25 -0400

    feature: added astro/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/edge";
-
 import svelte from "@astrojs/svelte";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [
+    tailwind(),
+    svelte(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   adapter: vercel(),
   output: "server",
 });
