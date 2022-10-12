438adcc4d732835944aa4a1ab9bccacc4dda98b3
Candido Sales Gomes <candidosg@gmail.com>
2022-10-12 17:12:38 -0400

    feature: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,13 @@ import vercel from "@astrojs/vercel/edge";
 import svelte from "@astrojs/svelte";
 import image from "@astrojs/image";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    svelte(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), svelte(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap()],
   adapter: vercel(),
-  output: "server",
+  output: "server"
 });
