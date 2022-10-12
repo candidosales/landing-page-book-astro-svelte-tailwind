a81b765ae7e86305464fa32b55414d63c6e20d23
Candido Sales Gomes <candidosg@gmail.com>
2022-10-12 00:45:23 -0400

    deploy to edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/edge";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  adapter: vercel(),
 });
