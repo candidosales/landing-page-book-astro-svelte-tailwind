686305f8135ac7c9031ebab8ac5b706c07cd0a03
Candido Sales Gomes <candidosg@gmail.com>
2023-08-30 15:13:24 -0300

    upgrade astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), partytown()],
-  adapter: vercel(),
-  output: "server"
+  integrations: [tailwind(), svelte(), sitemap(), partytown()],
+  output: "server",
 });
