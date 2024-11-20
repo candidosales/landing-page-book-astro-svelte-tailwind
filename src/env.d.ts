83aa5abc79c6493d7791a843027b260ed3c84213
Candido Sales Gomes <candidosg@gmail.com>
2024-11-19 21:06:27 -0500

    upgrade dps

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 // Replace `astro/client` with `@astrojs/image/client`
 /// <reference types="@astrojs/image/client" />
